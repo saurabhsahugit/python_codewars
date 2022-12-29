b4aa4162be875987836eb753638f4db7f18b8f15
saurabhsahugit <saurabh.newhorizons@gmail.com>
2022-12-29 13:29:40 +0000

    a more succinct solution

--- Are they the "same"?/Are_they_the_same.py
+++ Are they the "same"?/Are_they_the_same.py
@@ -1,28 +1,4 @@
-import math
-
 def comp(array1, array2):
-    result = []
-    
-    if array1 is None or array2 is None:
-        return False
-     
-    else:
-        for num in array2:
-            num = math.sqrt(num)
-            
-            if num in array1:
-                result.append("True")
-                array1.remove(num)
-            elif (num * -1) in array1:
-                result.append("True")
-                array1.remove(num * -1)
-            
-            else:
-                result.append("False")
-    
-    
-    if "False" in result:
-        return False
-    else:
-        return True
-	
+    if array1 and array2:
+        return sorted([x*x for x in array1]) == sorted(array2)
+    return array1 == array2 == []
